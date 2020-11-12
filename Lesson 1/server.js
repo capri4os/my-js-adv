6e2f8827b43adfc52c207f3a1910bd02818e1d3d
Ekaterina Erokhina <katerina.shimshek@gmail.com>
2020-11-12 12:23:44 +0300

    lesson 1

--- Lesson 1/server.js
+++ Lesson 1/server.js
@@ -5,8 +5,17 @@ const fs = require('fs');
 const server = http.createServer((req, res) => {
     console.log(req.url);
     res.end('Hello World!');
-    const body = fs.readFileSync('./index.html');
-    res.end(body);
+
+    const page = fs.readFileSync('./index.html');
+    res.end(page);
+
+    const style = fs.readFileSync('./style.css');
+    res.end(style);
+
+    const scenario = fs.readFile('./server.js');
+    res.end(scenario);
 })
+
 server.listen(process.env.PORT || 3000);
+
 console.log('Server started');
